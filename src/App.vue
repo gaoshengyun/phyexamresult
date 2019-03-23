6cb3d5bdb7aaf06a482410f27679e7c71c0d5280
gaoshengyun <18616242909@163.com>
2019-03-23 19:57:26 +0800

    导入导航组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav-list></nav-list>
     <router-view/>
   </div>
 </template>
@@ -27,3 +24,11 @@
   }
 }
 </style>
+<script>
+import NavList from './components/navlist'
+export default {
+  components:{
+    NavList
+  }
+}
+</script>
