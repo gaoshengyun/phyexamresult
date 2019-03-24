597ce2a0fcf322b6667b53d5b601a9f8c008dc11
gaoshengyun <18616242909@163.com>
2019-03-24 19:52:56 +0800

    配置路由

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-import NavList from './components/navlist'
+import BaseInfo from './components/baseinfo'
+
 
 Vue.use(Router)
 
@@ -9,20 +9,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path:'/eventlist',
-      component:NavList
+      component: BaseInfo
     }
   ]
 })
