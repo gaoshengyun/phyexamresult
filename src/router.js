9bcfed404ffc894a34a39a68ed8fe86389e191fa
gaoshengyun <18616242909@163.com>
2019-03-26 17:21:34 +0800

    添加血常规检查页面

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import BaseInfo from './components/baseinfo'
+import BloodCheck from './components/bloodcheck'
 
 
 Vue.use(Router)
@@ -10,6 +11,10 @@ export default new Router({
     {
       path: '/',
       component: BaseInfo
+    },
+    {
+      path:'/bloodcheck',
+      component:BloodCheck
     }
   ]
 })
