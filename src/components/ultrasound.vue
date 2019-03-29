4212dbadc1ab7729ab0bfd5cdc6094b01128789c
gaoshengyun <18616242909@163.com>
2019-03-29 20:28:28 +0800

    超声波检查

---
+++ src/components/ultrasound.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>超声波检查</h1>
+    <div class="result" v-for="(item, index) in result" :key="index.id">
+      <div v-html="item.result"></div>
+    </div>
+  </div>
+</template>
+<script>
+import {result} from '../mockdata/ultrasoundcheck.js'
+export default {
+  data() {
+    return {
+      result
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+  .result{
+    width: 900px;
+    margin: 0 auto;
+    text-align: left;
+  }
+</style>
