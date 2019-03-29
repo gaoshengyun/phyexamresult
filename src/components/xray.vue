f5c5ca8f6f540238f50fb9f3cb454d3933eb54f8
gaoshengyun <18616242909@163.com>
2019-03-29 20:29:11 +0800

    x射线检查

---
+++ src/components/xray.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <h1>胸透报告</h1>
+      <div class="result" v-html="result"></div>
+  </div>
+</template>
+<script>
+import {result} from '../mockdata/xray.js'
+export default {
+  data() {
+    return {
+      result
+    }
+  },
+}
+</script>
+<style lang="less" scoped>
+  .result{
+    text-align: left;
+    width: 900px;
+    margin: 0 auto;
+  }
+</style>
+
