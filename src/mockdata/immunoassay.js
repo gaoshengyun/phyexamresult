dd07cff5790d73056a5549fbf0275628eacfa57c
gaoshengyun <18616242909@163.com>
2019-03-31 15:05:36 +0800

    生化检查_尿检数据

---
+++ src/mockdata/immunoassay.js
@@ -0,0 +1,103 @@
+let result1 = [
+  {
+    id: 1,
+    abbreviation: 'U-LEU',
+    name: '尿白细胞',
+    result: '6',
+    hint: '',
+    reference: '<5',
+    units: ''
+  },
+  {
+    id: 2,
+    abbreviation: 'U-Ket',
+    name: '尿胴体',
+    result: '+',
+    hint: '↑',
+    reference: '阴性(-)',
+    units: ''
+  },
+  {
+    id: 3,
+    abbreviation: 'NIT',
+    name: '尿亚硝酸盐',
+    result: '+',
+    hint: '↑',
+    reference: '阴性(-)',
+    units: ''
+  },
+  {
+    id: 4,
+    abbreviation: 'URO',
+    name: '尿胆原',
+    result: '弱阳性',
+    hint: '',
+    reference: '弱阳性',
+    units: ''
+  },
+  {
+    id: 5,
+    abbreviation: 'U-BIL',
+    name: '尿胆红素',
+    result: '-',
+    hint: '',
+    reference: '阴性(-)',
+    units: ''
+  },
+  {
+    id: 6,
+    abbreviation: 'PRO',
+    name: '尿蛋白',
+    result: '+',
+    hint: '↑',
+    reference: '阴性(-)',
+    units: ''
+  },
+  {
+    id: 7,
+    abbreviation: 'GLU',
+    name: '尿糖',
+    result: '-',
+    hint: '',
+    reference: '阴性(-)',
+    units: ''
+  },
+  {
+    id: 8,
+    abbreviation: 'SG',
+    name: '尿比重',
+    result: '1.028',
+    hint: '↑',
+    reference: '1.015~1.025',
+    units: ''
+  },
+  {
+    id: 9,
+    abbreviation: 'PH',
+    name: '尿酸咸度',
+    result: '6.7',
+    hint: '',
+    reference: '4.5~8.0',
+    units: ''
+  },
+  {
+    id: 10,
+    abbreviation: 'BLO',
+    name: '隐血',
+    result: '-',
+    hint: '',
+    reference: '阴性(-)',
+    units: ''
+  },
+  {
+    id: 11,
+    abbreviation: 'VC',
+    name: '维C',
+    result: '-',
+    hint: '',
+    reference: '阴性(-)',
+    units: ''
+  }    
+]
+
+export {result1}
