65a7c714a4473b4c42c341fad6f047775e37dad7
gaoshengyun <18616242909@163.com>
2019-04-02 19:17:40 +0800

    生化检查信息补全

--- src/components/bloodcheck.vue
+++ src/components/bloodcheck.vue
@@ -3,6 +3,9 @@
     <h1>血清检查一</h1>
     <table>
       <tr>
+        <th class="info" colspan="6">姓名:高胜云 &nbsp;&nbsp;&nbsp;性别:男 &nbsp;&nbsp;&nbsp; 年龄:35 &nbsp;&nbsp;&nbsp;送检日期:2019-03-28</th>
+      </tr>
+      <tr>
         <th>简称</th>
         <th>项目</th>
         <th>结果</th>
@@ -19,11 +22,19 @@
         <td>{{item.reference}}</td>
         <td>{{item.units}}</td>
       </tr>
-    
+
+      <tr>
+        <th class="info" colspan="3">体检执行编号:ITSY9088465812</th>
+        <th class="info" colspan="3">血清检查执行编号:ITSY-BBT-779016484</th>
+      </tr>
     </table>
+    <div class="tips">* 根据体检执行编号和血清检查执行编号可追溯检验医师相关信息及打印原始报告单据</div>
 
     <h1>血清检查二</h1>
     <table>
+      <tr>
+        <th class="info" colspan="6">姓名:高胜云 &nbsp;&nbsp;&nbsp;性别:男 &nbsp;&nbsp;&nbsp; 年龄:35 &nbsp;&nbsp;&nbsp;送检日期:2019-03-28</th>
+      </tr>
       <tr>
         <th>简称</th>
         <th>项目</th>
@@ -40,10 +51,18 @@
         <td>{{item.reference}}</td>
         <td>{{item.units}}</td>
       </tr>
+      <tr>
+        <th class="info" colspan="3">体检执行编号:ITSY9088465812</th>
+        <th class="info" colspan="3">血清检查执行编号:ITSY-BBT-779033798</th>
+      </tr>
     </table>
+    <div class="tips">* 根据体检执行编号和血清检查执行编号可追溯检验医师相关信息及打印原始报告单据</div>
 
     <h1>全血检查一</h1>
     <table>
+      <tr>
+        <th class="info" colspan="6">姓名:高胜云 &nbsp;&nbsp;&nbsp;性别:男 &nbsp;&nbsp;&nbsp; 年龄:35 &nbsp;&nbsp;&nbsp;送检日期:2019-03-28</th>
+      </tr>
       <tr>
         <th>简称</th>
         <th>项目</th>
@@ -60,10 +79,18 @@
         <td>{{item.reference}}</td>
         <td>{{item.units}}</td>
       </tr>
+      <tr>
+        <th class="info" colspan="3">体检执行编号:ITSY9088465812</th>
+        <th class="info" colspan="3">血清检查执行编号:ITSY-BBC-2335337790</th>
+      </tr>
     </table>
+    <div class="tips">* 根据体检执行编号和血清检查执行编号可追溯检验医师相关信息及打印原始报告单据</div>
 
     <h1>全血检查二</h1>
     <table>
+      <tr>
+        <th class="info" colspan="6">姓名:高胜云 &nbsp;&nbsp;&nbsp;性别:男 &nbsp;&nbsp;&nbsp; 年龄:35 &nbsp;&nbsp;&nbsp;送检日期:2019-03-28</th>
+      </tr>
       <tr>
         <th>简称</th>
         <th>项目</th>
@@ -80,7 +107,12 @@
         <td>{{item.reference}}</td>
         <td>{{item.units}}</td>
       </tr>
+      <tr>
+        <th class="info" colspan="3">体检执行编号:ITSY9088465812</th>
+        <th class="info" colspan="3">血清检查执行编号:ITSY-BBC-233697033</th>
+      </tr>
     </table>
+    <div class="tips">* 根据体检执行编号和血清检查执行编号可追溯检验医师相关信息及打印原始报告单据</div>
   </div>
 </template>
 <script>
@@ -106,6 +138,11 @@ export default {
     border-bottom: 1px solid #000;
     th{
       border: 1px solid #000;
+      &.info{
+        text-align: left;
+        padding-left: 10px;
+        height: 30px;
+      }
     }
     td{
       text-align: left;
@@ -116,6 +153,12 @@ export default {
         font-weight: bold;
       }
     }
+  }
+  .tips{
+    text-align: left;
+    width: 900px;
+    margin: 0 auto;
+    padding: 10px;
   }
 </style>
 
