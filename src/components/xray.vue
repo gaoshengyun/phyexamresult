b49d37a1d1dcdf72016b0bcaf74348da239032f4
gaoshengyun <18616242909@163.com>
2019-04-04 15:47:42 +0800

    样式优化

--- src/components/xray.vue
+++ src/components/xray.vue
@@ -3,6 +3,7 @@
       <h1>胸透报告</h1>
       <div class="uname">姓名:高胜云    性别:男     年龄:35    检查日期:2019-03-28</div>
       <div class="result" v-html="result"></div>
+      <div class="conclusion">结论:所检项目均正常</div>
       <div class="serialNumber">体检执行编号:ITSY9088465812 超声波执行编号:ITSY-XRAY-796005530</div>
       <div class="tips">* 根据体检执行编号和血清检查执行编号可追溯检验医师相关信息及打印原始报告单据</div>
   </div>
@@ -23,11 +24,15 @@ export default {
     width: 900px;
     margin: 0 auto;
   }
-  .tips,.serialNumber{
+  .tips,.serialNumber,.conclusion{
     text-align: left;
     width: 900px;
     margin: 0 auto;
     padding: 10px;
+  }
+  .conclusion{
+    font-weight: 700;
+    font-size: 20px;
   }
   .uname{
     font-weight: 700;
